2ba8f7f1c17c9331928f1bff7be1f2b94885230b
Alexandre H Mandri <alexandre.mandri@gmail.com>
2026-05-09 20:10:02 -0300

    Importando e usando arquivos de CSS Modules com escopo de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Heading } from './components/Heading';
+
+import './styles/global.css';
+import './styles/theme.css';
+
 export function App() {
     return (
         <>
-            <h1>Olá, Mundo!</h1>
+            <Heading/>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, sit voluptas eaque quo repellendus tempore reprehenderit, eos et laudantium, corporis sapiente blanditiis eum? Dignissimos, saepe pariatur iusto vero inventore deserunt.</p>
         </>
     );
