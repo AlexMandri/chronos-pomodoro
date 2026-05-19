cc955d743f5a2623055772669fe99ecd42139c13
Alexandre H Mandri <alexandre.mandri@gmail.com>
2026-05-19 09:41:19 -0300

    Movendo o Container para seu próprio React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
     return (
         <>
-            <div className='container'>
-                <div className="content">
-                    <section>
-                       LOGO
-                    </section>
-                </div>
-            </div>
-            <div className='container'>
-                <div className="content">
-                    <section>
-                        MENU
-                    </section>
-                </div>
-            </div>
-            <div className='container'>
-                <div className="content">
-                    <section>
-                        FORM
-                    </section>
-                </div>
-            </div>
-            <div className='container'>
-                <div className="content">
-                    <section>
-                        FOOTER
-                    </section>
-                </div>
-            </div>
-           
-            {/* <div className='container-fluid'>
-                <div className="container">
-                    <div className="content">
-                        <section>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam ipsam esse soluta velit quae quasi iure. Ab eligendi adipisci magni ad, sequi ipsam? In molestias aut dolores! Quidem, ad.
-                        </section>
-                    </div>
-                </div>
-            </div> */}
+            <Container>
+                <Heading>Logo</Heading>
+            </Container>
+            <Container>
+                <Heading>Menu</Heading>
+            </Container>
+          
 
         </>
     );
